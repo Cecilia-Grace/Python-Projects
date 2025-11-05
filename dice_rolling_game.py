c40f14b6bd045b3f6607d6b609ef584a5bd40076
Cecilia Grace <cecigrace2003@gmail.com>
2025-11-06 01:40:03 +0300

    initial commit

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,13 +1,28 @@
 import random
 
+rolls = []
 while True:
     user_input = input("Roll the dice(y/n)? ").lower()
-
+    
     if user_input == "y":
-        print(random.randint(1, 6), random.randint(1, 6))
-        user_input 
+        rolls.append(1)
+        numbers_of_dice = int(input("How many dice do you want to roll? "))
+        print(f"You chose {numbers_of_dice} dice.")
+        for i in range(numbers_of_dice):
+            print(f"You rolled and got: {random.randint(1, 6)}")
+            i +=1
+            user_input 
     elif user_input == "n":
-        print("Thanks for playing!")
+        print(f"You have rolled the dice: {len(rolls)} time(s).")
+        print("Thanks for playing!") 
         break
     else:
         print("Invalid Choice!")
+        
+    
+                    
+                
+        
+
+        
+         
